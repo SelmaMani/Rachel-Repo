c91d19f20c79fa722be55196a52b0f46294384fa
SamahFarez <samah.farez@gmail.com>
2024-11-29 10:13:38 +0100

    Hosted

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -15,7 +15,7 @@ const LoginPage = () => {
         const userData = { email, password };
 
         try {
-            const response = await fetch('http://localhost:5000/login', {
+            const response = await fetch('https://recettemagique.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
